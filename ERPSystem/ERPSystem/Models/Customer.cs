03a7da052949007f5a00d3ed20a33bc0dbdb86f1
Dominik Göggerle <goeggerle@theobald.local>
2021-08-09 14:08:28 +0200

    Added customer model

---
+++ ERPSystem/ERPSystem/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPSystem.Models
+{
+    public class Customer
+    {
+        public string CustomerNo { get; set; }
+        public string CustomerName { get; set; }
+        public string Streetaddress { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public bool VIP { get; set; }
+    }
+}
