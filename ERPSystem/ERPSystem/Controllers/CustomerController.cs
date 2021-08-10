0805850cac4b337cdee64e6074dc5961f448c168
Oleksandra Prokhorova <oleksandra.prokhorova@theobald-software.com>
2021-08-10 09:02:01 +0200

    separated customer controller

---
+++ ERPSystem/ERPSystem/Controllers/CustomerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ERPSystem.Helpers;
+using ERPSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERPSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        
+        public async Task<IActionResult> Index()
+        {
+            var customers = await SqlHelper.GetCustomersAsync(CancellationToken.None);
+            return View(customers);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUpdateCustomer(Customer customer)
+        {
+            if (customer.CustomerNo == null)
+            {
+                var existingCustomers = await SqlHelper.GetCustomersAsync(CancellationToken.None);
+
+                int nextCustomerNumber = 1;
+
+                while(nextCustomerNumber < 10000000)
+                {
+                    if(!existingCustomers.Any(c => c.CustomerNo == nextCustomerNumber.ToString()))
+                    {
+                        customer.CustomerNo = nextCustomerNumber.ToString();
+                        break;
+                    }
+
+                    nextCustomerNumber++;
+                }
+            
+                await SqlHelper.AddCustomerAsync(customer, CancellationToken.None);
+            }
+            else
+            {
+                await SqlHelper.UpdateCustomerAsync(customer, CancellationToken.None);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> AddEditCustomer(string CustomerNo)
+        {
+            var existingCustomers = await SqlHelper.GetCustomersAsync(CancellationToken.None);
+
+            var model = existingCustomers.FirstOrDefault(c => c.CustomerNo == CustomerNo) ?? new Customer();
+
+            return PartialView("CustomerDetails", model);
+        }
+        
+        public async Task<IActionResult> DeleteCustomer(string customerNo)
+        {
+            await SqlHelper.DeleteCustomerAsync(customerNo, CancellationToken.None);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- ERPSystem/ERPSystem/Controllers/HomeController.cs
+++ ERPSystem/ERPSystem/Controllers/HomeController.cs
@@ -30,46 +30,7 @@ namespace ERPSystem.Controllers
 
             return View(orders);
         }
-        public async Task<IActionResult> Customers()
-        {
-            var customers = await SqlHelper.GetCustomersAsync(CancellationToken.None);
-            return View(customers);
-        }
-
-        public IActionResult AddCustomer()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddCustomer(Customer customer)
-        {
-            var existingCustomers = await SqlHelper.GetCustomersAsync(CancellationToken.None);
-
-            int nextCustomerNumber = 1;
-
-            while(nextCustomerNumber < 10000000)
-            {
-                if(!existingCustomers.Any(c => c.CustomerNo == nextCustomerNumber.ToString()))
-                {
-                    customer.CustomerNo = nextCustomerNumber.ToString();
-                    break;
-                }
-
-                nextCustomerNumber++;
-            }
-            
-            await SqlHelper.AddCustomerAsync(customer, CancellationToken.None);
-
-            return RedirectToAction("Customers");
-        }
         
-        public async Task<IActionResult> DeleteCustomer(string customerNo)
-        {
-            await SqlHelper.DeleteCustomerAsync(customerNo, CancellationToken.None);
-
-            return RedirectToAction("Customers");
-        }
 
         public IActionResult AddOrder()
         {
