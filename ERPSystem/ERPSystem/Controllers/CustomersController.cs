ab144c87d6b1b6751d8735e522c0cf6aa0eac512
Oleksandra Prokhorova <oleksandra.prokhorova@theobald-software.com>
2021-08-10 09:20:54 +0200

    customer controller rename to customers

--- ERPSystem/ERPSystem/Controllers/CustomersController.cs
+++ ERPSystem/ERPSystem/Controllers/CustomersController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ERPSystem.Controllers
 {
-    public class CustomerController : Controller
+    public class CustomersController : Controller
     {
         
         public async Task<IActionResult> Index()
